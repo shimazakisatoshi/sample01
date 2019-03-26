5df1ccb7a6777e3cce2be00d0200b755e78de801
shimazakisatoshi <nihonkaisodati3104@outlook.jp>
2019-03-26 11:03:45 +0900

    １から９までの合計を追加

--- Gitsample02/src/Gitsample/pkg/Gitsamplemain02.java
+++ Gitsample02/src/Gitsample/pkg/Gitsamplemain02.java
@@ -5,6 +5,13 @@ public class Gitsamplemain02 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("hello git");
+
+		int num=0;
+
+		for (int i = 0; i <=9; i++) {
+			num+=i;
+		}
+		System.out.println(num);
 	}
 
 }
